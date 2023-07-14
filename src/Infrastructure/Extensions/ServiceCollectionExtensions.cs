6f02cc3d15da195a79585fdc3ebf948aa545839c
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2023-07-14 05:53:53 +0000

    Ajout du service Transient AddTransient<ITodoRepository, TodoRepository>()

--- src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -28,6 +28,7 @@ namespace BlazorHero.CleanArchitecture.Infrastructure.Extensions
                 .AddTransient(typeof(IRepositoryAsync<,>), typeof(RepositoryAsync<,>))
                 .AddTransient<IProductRepository, ProductRepository>()
                 .AddTransient<IBrandRepository, BrandRepository>()
+                .AddTransient<ITodoRepository, TodoRepository>()
                 .AddTransient<IDocumentRepository, DocumentRepository>()
                 .AddTransient<IDocumentTypeRepository, DocumentTypeRepository>()
                 .AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
