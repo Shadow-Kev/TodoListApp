cdf742ad32324ad84c9b83aa687825efa96bbc86
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2023-07-14 10:25:35 +0000

    Ajout de GetAllAsync() dans le Manager

---
+++ src/Client.Infrastructure/Managers/TodoListApp/Todo/ITodoManager.cs
@@ -0,0 +1,12 @@
+﻿using BlazorHero.CleanArchitecture.Application.Features.Todos.Queries.GetAll;
+using BlazorHero.CleanArchitecture.Shared.Wrapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Client.Infrastructure.Managers.TodoListApp.Todo
+{
+    public interface ITodoManager : IManager
+    {
+        Task<IResult<List<GetAllTodosResponse>>> GetAllAsync();
+    }
+}
---
+++ src/Client.Infrastructure/Managers/TodoListApp/Todo/TodoManager.cs
@@ -0,0 +1,23 @@
+﻿using BlazorHero.CleanArchitecture.Application.Features.Todos.Queries.GetAll;
+using BlazorHero.CleanArchitecture.Client.Infrastructure.Extensions;
+using BlazorHero.CleanArchitecture.Shared.Wrapper;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Client.Infrastructure.Managers.TodoListApp.Todo
+{
+    public class TodoManager : ITodoManager
+    {
+        private readonly HttpClient _httpClient;
+        public TodoManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IResult<List<GetAllTodosResponse>>> GetAllAsync()
+        {
+            var response = await _httpClient.GetAsync(Routes.TodosEndpoint.GetAll);
+            return await response.ToResult<List<GetAllTodosResponse>>();
+        }
+    }
+}
