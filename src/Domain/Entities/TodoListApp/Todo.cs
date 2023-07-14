a86565209ebe699ff35e48ee6cdb0859006eaa67
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2023-07-14 05:36:55 +0000

    Création de l'entité todo

---
+++ src/Domain/Entities/TodoListApp/Todo.cs
@@ -0,0 +1,18 @@
+﻿using BlazorHero.CleanArchitecture.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Domain.Entities.TodoListApp
+{
+    public class Todo : AuditableEntity<int>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int Priority { get; set; }
+        public bool IsCompleteted { get; set; }
+    }
+}
