bec8890c680e9ccc072658d943cc75355c2de6c7
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2023-07-14 05:49:54 +0000

    Création de la classe TodoRepository

---
+++ src/Infrastructure/Repositories/TodoRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlazorHero.CleanArchitecture.Application.Interfaces.Repositories;
+using BlazorHero.CleanArchitecture.Domain.Entities.TodoListApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Infrastructure.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private readonly IRepositoryAsync<Todo, int> _todoRepository;
+        public TodoRepository(IRepositoryAsync<Todo, int> todoRepository) => _todoRepository = todoRepository;
+
+    }
+}
